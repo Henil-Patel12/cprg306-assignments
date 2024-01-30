5a37bcacd6122903fcc412556f5243891728027d
Henil-Patel12 <henilkumar.patel@edu.sait.ca>
2024-01-29 18:16:02 -0700

    Final Submission.

--- app/week-2/studen-info.js
+++ app/week-2/studen-info.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function StudentInfo() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center ">
       <p>Henilkumar Patel</p>
       <Link href="https://github.com/Henil-Patel12/cprg306-assignments">GitHub Repository Link</Link>
     </div>
