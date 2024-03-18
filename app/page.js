e88a2ca4d75e3b4b069ff8d6a9301d4fd8c3ec07
Henil-Patel12 <henilkumar.patel@edu.sait.ca>
2024-03-18 13:16:33 -0600

    week 8 done

--- app/page.js
+++ app/page.js
@@ -21,6 +21,9 @@ export default function Page() {
         <li className="text-orange-500 hover:text-purple-400 hover:underline mb-3" style={{ backgroundColor: 'hsl(0, 0%, 4%)', borderRadius: '10px', padding: '10px', boxShadow: '0 50px 50px 0 rgba(0, 0, 0, 0.2)' }}>
           <a href="/week-7">Assignment Week-7</a>
         </li>
+        <li className="text-orange-500 hover:text-purple-400 hover:underline mb-3" style={{ backgroundColor: 'hsl(0, 0%, 4%)', borderRadius: '10px', padding: '10px', boxShadow: '0 50px 50px 0 rgba(0, 0, 0, 0.2)' }}>
+          <a href="/week-8">Assignment Week-8</a>
+        </li>
       </div>
     </main>
   );
